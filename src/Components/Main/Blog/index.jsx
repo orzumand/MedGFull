dfc6589dfe2825292e70d6d90484c5a41dd83d03
Orzumand <hayotbekyoldashev0@gmail.com>
2022-12-16 22:16:19 +0500

    Main page design complate

--- src/Components/Main/Blog/index.jsx
+++ src/Components/Main/Blog/index.jsx
@@ -1,13 +1,90 @@
 import React from "react";
 
-import { Container, Wrapper } from "./style";
+import {
+  Card,
+  CardTitle,
+  Container,
+  Content,
+  Img,
+  Left,
+  Right,
+  Wrapper,
+} from "./style";
 // import Partners from "../../../Assets/Img/Partner.svg";
-
+import Blogs from "../../../Assets/Img/Blog.svg";
 const Blog = () => {
   return (
     <Wrapper>
       <Container>
         <div className="title">Блог о здоровье</div>
+        <Content>
+          <Left>
+            <Img src={Blogs} />
+            <div className="title">
+              Стома: уход, калоприемники и их использование, диета, полезные
+              советы
+            </div>
+            <div className="subtitle">
+              Искусственное отверстие в кишке, сформированное хирургическим
+              путем после удаления всего кишечника или его части, которое
+              выведено на переднюю брюшную...
+            </div>
+          </Left>
+          <Right>
+            <Card>
+              <Img src={Blogs} />
+              <CardTitle>
+                <div className="title">
+                  Стома: уход, калоприемники и их использование, диета, полезные
+                </div>
+                <div className="subtitle">
+                  Искусственное отверстие в кишке, сформированное хирургическим
+                  путем после удаления всего кишечника или его части, которое
+                  выведено на переднюю брюшную...
+                </div>
+              </CardTitle>
+            </Card>
+            <Card>
+              <Img src={Blogs} />
+              <CardTitle>
+                <div className="title">
+                  Стома: уход, калоприемники и их использование, диета, полезные
+                </div>
+                <div className="subtitle">
+                  Искусственное отверстие в кишке, сформированное хирургическим
+                  путем после удаления всего кишечника или его части, которое
+                  выведено на переднюю брюшную...
+                </div>
+              </CardTitle>
+            </Card>
+            <Card>
+              <Img src={Blogs} />
+              <CardTitle>
+                <div className="title">
+                  Стома: уход, калоприемники и их использование, диета, полезные
+                </div>
+                <div className="subtitle">
+                  Искусственное отверстие в кишке, сформированное хирургическим
+                  путем после удаления всего кишечника или его части, которое
+                  выведено на переднюю брюшную...
+                </div>
+              </CardTitle>
+            </Card>
+            <Card>
+              <Img src={Blogs} />
+              <CardTitle>
+                <div className="title">
+                  Стома: уход, калоприемники и их использование, диета, полезные
+                </div>
+                <div className="subtitle">
+                  Искусственное отверстие в кишке, сформированное хирургическим
+                  путем после удаления всего кишечника или его части, которое
+                  выведено на переднюю брюшную...
+                </div>
+              </CardTitle>
+            </Card>
+          </Right>
+        </Content>
       </Container>
     </Wrapper>
   );
--- src/Components/Main/Blog/style.js
+++ src/Components/Main/Blog/style.js
@@ -31,8 +31,94 @@ const Container = styled.div`
 `;
 
 const Img = styled.img`
-  border-top-left-radius: 12px;
-  border-top-right-radius: 12px;
+  border-radius: 28px;
 `;
 
-export { Wrapper, Img, Container };
+const Content = styled.div`
+  height: 590px;
+  width: 100%;
+  gap: 20px;
+  display: flex;
+`;
+const Left = styled.div`
+  flex: 1;
+
+  Img {
+    height: 383px;
+    width: 100%;
+  }
+  .title {
+    margin-top: 24px;
+    margin-bottom: 16px;
+    font-family: "Inter";
+    font-style: normal;
+    font-weight: 600;
+    font-size: 25px;
+    line-height: 30px;
+
+    /* Light/black */
+
+    color: #402c36;
+  }
+  .subtitle {
+    font-family: "Inter";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 15px;
+    line-height: 130%;
+    /* or 19px */
+
+    /* Light/sec */
+
+    color: #c2b7bd;
+  }
+`;
+const Right = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+const Card = styled.div`
+  display: flex;
+  gap: 20px;
+  width: 100%;
+  height: 120px;
+
+  Img {
+    height: 120px;
+    width: 183px;
+  }
+`;
+const CardTitle = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  .title {
+    font-family: "Inter";
+    font-style: normal;
+    font-weight: 600;
+    font-size: 19px;
+    line-height: 23px;
+
+    /* Light/black */
+
+    color: #402c36;
+    margin-bottom: 8px;
+  }
+  .subtitle {
+    font-family: "Inter";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 13px;
+    line-height: 130%;
+    /* or 17px */
+
+    /* Light/sec */
+
+    color: #c2b7bd;
+  }
+`;
+export { Wrapper, Img, Container, Content, Left, Right, Card, CardTitle };
